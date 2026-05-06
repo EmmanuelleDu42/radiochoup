06afe6adb02fed740758de32d8b60ed4511b5e35
Gabriel <gabriel-prata@phaseneutre.fr>
2026-05-06 19:33:08 +0200

    feat(ui): Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+export function Footer() {
+  return (
+    <footer className="mt-8 bg-[url('/img/background_footer.png')] bg-repeat py-4 text-center text-xs text-choup-pink-100">
+      <a href="https://www.mariepierrepastini.fr/" rel="external noreferrer" target="_blank" className="hover:underline">
+        Webdesign MP Pastini
+      </a>{" "}
+      —{" "}
+      <a href="#" className="hover:underline">
+        Développement Cef-i
+      </a>{" "}
+      —{" "}
+      <span>Radio Choup, tous droits réservés</span>
+    </footer>
+  );
+}
